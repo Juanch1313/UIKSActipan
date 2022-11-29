f9516b4f5f31771794e5d9a6c8d73444f22b4031
Jhonny <juanchitorodi1312@gmail.com>
2022-11-28 19:23:38 -0600

    Project Images

--- UIKSActipan/Program.cs
+++ UIKSActipan/Program.cs
@@ -16,7 +16,7 @@ namespace UIKSActipan
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Login());
+            Application.Run(new IndexW());
         }
     }
 }
